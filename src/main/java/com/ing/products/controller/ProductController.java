e8f2330864aede5e072c82a6f22c8f76b5bebdee
nithin2889 <nithin2889@gmail.com>
2019-06-06 00:55:55 -0700

    product list committed

--- src/main/java/com/ing/products/controller/ProductController.java
+++ src/main/java/com/ing/products/controller/ProductController.java
@@ -1,8 +1,32 @@
 package com.ing.products.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ing.products.pojo.ProductDTO;
+import com.ing.products.service.ProductService;
+
 
 @RestController
 public class ProductController {
+	@Autowired
 
+    ProductService service;
+
+    
+
+    @GetMapping("/products/{groupid}")
+
+    public ResponseEntity<List<ProductDTO>> getProducts(@PathVariable("groupid") long id) {
+    	
+        List<ProductDTO> product = service.findByGroupId(id);
+        return new ResponseEntity<List<ProductDTO>>(product , HttpStatus.OK);   
+
+    }
 }
--- src/main/java/com/ing/products/repository/ProductRepository.java
+++ src/main/java/com/ing/products/repository/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.ing.products.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.ing.products.entity.Product;
+import com.ing.products.pojo.ProductDTO;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+	@Query(value = "select * from products where group_Id=?1",nativeQuery = true)
+    List<ProductDTO> findByGroupId(long id);
 }
